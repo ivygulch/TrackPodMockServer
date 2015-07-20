085cc0700992b6c417af2ac028626745644211b0
Ryan Sjoquist <ryan.sjoquist@gmail.com>
2015-07-20 11:58:03 -0400

    updated server scan

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ var bodyParser = require('body-parser')
 var sessions101 = require('./routes/sessions101');
 var sessions102 = require('./routes/sessions102');
 var sessions103 = require('./routes/sessions103');
-var userScan = require('./routes/userScan');
+var users = require('./routes/users');
+var scan = require('./routes/scan');
 app.use(bodyParser());
 app.disable('etag');
 
@@ -15,7 +16,8 @@ app.get('/events/earliestEndDate/*', events.events);
 app.get('/events/101/products/earliestEndDate/*', sessions101.sessions101);
 app.get('/events/102/products/earliestEndDate/*', sessions102.sessions102);
 app.get('/events/103/products/earliestEndDate/*', sessions103.sessions103);
-app.get('products/101/session_scans/individuals/%@', userScan.userScan);
+app.get('/products/101/session_scans/individuals/*', users.users);
+app.post('/products/101/session_scans', scan.scan);
 app.get('/test', function(req,res) {
   res.send('This worked');
 });
@@ -25,6 +27,6 @@ var server = app.listen(3000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log('Example app listening at http://%s:%s', host, port);
+    console.log('Example app listening at http://%:%s', host, port);
 
 });
