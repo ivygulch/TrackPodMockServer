8a959c94c9890f3aa7de375906a758aee2eabdbc
Douglas Sjoquist <dwsjoquist@sunetos.com>
2015-10-12 11:31:26 -0400

    fix products/#/session_scans/individuals/# call in dummy server

--- routes/users.js
+++ routes/users.js
@@ -5,21 +5,13 @@
  message: {string},
  result: [
  {
- indID:{indID},
- key:{key},
- externalKey":{externalKey},
- badgeNumber":{badgeNumber},
- firstName":{firstName},
- lastName":{lastName},
- emailAddress":{emailAddress},
- companyName":{companyName},
- lastScannedOn":{lastScannedOn},
- direction":{direction}
- }, {
- prodID: {integer},
- externalKey: {string},
- code: {string},
- */
+ badgeNumber: {string},
+ direction: {string},
+ scannedOn: {timestamp},
+ override: {boolean}
+ } ]
+ }
+   */
 exports.users = function(req, res) {
     var responseJSON = {
         success:true,
@@ -27,44 +19,13 @@ exports.users = function(req, res) {
         message:"users",
         result:[
             {
-                indID:1,
-                key:"key101",
-                externalKey:"key101",
-                badgeNumber:"DummyGoodID",
-                firstName:"1ryan",
-                lastName:"Sjoquist",
-                emailAddress:"exe@gmail.com",
-                companyName:"sunetos",
-                lastScannedOn:"2015-06-01T09:00:00GMT",
-                direction:"in"
-                },
-            {
-                indID:2,
-                key:"key102",
-                externalKey:"key102",
-                badgeNumber:"DummyGoodID",
-                firstName:"2ryan",
-                lastName:"Sjoquist",
-                emailAddress:"exe@gmail.com",
-                companyName:"sunetos",
-                lastScannedOn:"2015-07-01T09:00:00GMT",
-                direction:"out"
-            },
-            {
-                indID:3,
-                key:"key103",
-                externalKey:"key103",
-                badgeNumber:"DummyGoodID",
-                firstName:"3ryan",
-                lastName:"Sjoquist",
-                emailAddress:"exe@gmail.com",
-                companyName:"sunetos",
-                lastScannedOn:"2015-08-01T09:00:00GMT",
-                direction:"out"
+                badgeNumber:"DummyScanTwiceID",
+                direction:"in",
+                scannedOn:"2015-06-01T09:00:00GMT",
+                override:false
             }
         ]
         };
     res.contentType("application/json");
     res.send(JSON.stringify(responseJSON),200);
 }
-
